78ac8fbc7e12a2c57b81e2c93aca08a6d1375efc
ardhaniahlan <ardhan.official16@gmail.com>
2026-09-08 19:38:53 +0700

    Fix

--- src/features/auth/auth.controller.ts
+++ src/features/auth/auth.controller.ts
@@ -30,7 +30,7 @@ export const login = async (req: Request, res: Response) => {
 
     res
       .status(200)
-      .json({ success: true, message: "Login berhasil", data: { user } });
+      .json({ success: true, message: "Login berhasil", data: { user, token } });
   } catch (error: any) {
     res.status(401).json({ success: false, message: error.message });
   }
